218af879b8337615d303bad48c65997a869897c9
bgts <390676317@qq.com>
2014-12-05 22:58:28 +0800

    Revert "Revert "initial""
    This reverts commit 148409cf4c21a1e2b079738543aaa7ce87b4002a.

---
+++ src/model/UploadAction.java
@@ -0,0 +1,80 @@
+package model;
+
+import java.io.File;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.struts2.ServletActionContext;
+
+public class UploadAction {
+	private File upload;
+	// 封装单个上传文件类型的属性
+	private String uploadContentType;
+	// 封装单个上传文件名的属性
+	private String uploadFileName;
+	HttpServletRequest request = ServletActionContext.getRequest();
+	String URLPath;
+	public String getSavePath() {
+		// return ServletActionContext.getRequest().getRealPath("");
+		String onload = "C:\\report\\cached\\";
+		request.setAttribute("onload", onload);
+		// return ServletActionContext.getRequest().getContextPath();
+		return onload;
+	}
+
+	public void setSavePath(String savePath) {
+	}
+
+	// 上传单个文件的文件类型的setter和getter方法
+	public void setUploadContentType(String uploadContentType) {
+		this.uploadContentType = uploadContentType;
+	}
+
+	public String getUploadContentType() {
+		return (this.uploadContentType);
+	}
+
+	// 上传单个文件的文件名的setter和getter方法
+	public void setUploadFileName(String uploadFileName) {
+		this.uploadFileName = uploadFileName;
+	}
+
+	public String getUploadFileName() {
+		return (this.uploadFileName);
+	}
+
+	public File getUpload() {
+		return upload;
+	}
+
+	public void setUpload(File upload) {
+		this.upload = upload;
+		// savePath = ServletActionContext .getRequest().getRealPath("");
+	}
+
+	// 上传单个文件
+	public String execute() {
+		try {
+			URLPath=request.getScheme() + "://"
+					+ request.getServerName() + ":" + request.getServerPort()
+					+request.getContextPath()+"/upload/"+getUploadFileName();
+			String RealPath = ServletActionContext.getServletContext()
+					.getRealPath("/upload")
+					+ File.separator
+					+ getUploadFileName();
+			// 以服务器的文件保存地址和原文件名建立上传文件输出流
+			System.out.println(RealPath + "路径");
+			File fos = new File(RealPath);
+			FileUtils.copyFile(upload, fos);
+			System.out.println(URLPath);
+				FromTaobao crawler = new FromTaobao();
+				crawler.addSeed(URLPath);
+				crawler.addRegex(URLPath);
+				crawler.start(1);
+			// 以上传文件建立一个文件上传流
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "success";
+	}
+}
